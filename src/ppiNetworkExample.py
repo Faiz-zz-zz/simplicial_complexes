e948e6bd950203480179f45713c0674d9c116f22
Mo Afshar <moafshaar@gmail.com>
2018-01-21 19:22:23 +0000

    PPI network example using NetworkX library

---
+++ src/ppiNetworkExample.py
@@ -0,0 +1,26 @@
+import networkx as nx 
+import pandas as pd
+import matplotlib.pyplot as plt
+import community 
+
+def basicNetwork(): 
+
+	df = pd.read_csv("BIOGRID-Homosapien")
+	nodesA = df['Entrez Gene Interactor A']
+	nodesB = df['Entrez Gene Interactor B']
+	nodes = pd.concat([nodesA, nodesB]) #get a list of all the nodes in the network
+	allNodes = list(set(nodes)) #remove any duplicates, for the case if a gene is repeated
+	#print(allNodes)
+
+	edges = list(zip(df['Entrez Gene Interactor A'], df['Entrez Gene Interactor B'])) #get a list of all the edges
+	#print(edges)
+
+	##Also possible to add weighted edges as well adding nodes and edges one-at-a-time
+	##https://networkx.github.io/documentation/stable/tutorial.html
+	G = nx.Graph()
+	G.add_nodes_from(allNodes)
+	G.add_edges_from(edges)
+
+	print(nx.info(G)) #Gives basic info of the graph, number of nodes, edges and the average degree
+
+basicNetwork()
