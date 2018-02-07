9e96c589b9a2db7ed0034357158873702b0ae85a
Shivam Shah <Shivam Shah>
2018-02-07 14:45:17 +0000

    Computed degree,closeness, betweenness centralities for PPI network, and node degrees per node in PPI network

--- src/measures.py
+++ src/measures.py
@@ -1,10 +1,17 @@
+import networkx as nx
+import pandas as pd
+from tqdm import tqdm
+
+
 def find_shortest_path():
     pass
 
 
-def generate_PPI_network(data_set):
-    data_set_location = "../raw_data/"
-    df = pd.read_csv(data_set_location + data_set + ".csv")
+def generate_PPI_network(file_path):
+    #need to redo this indexing part 
+
+    # data_set_location = "../raw_data/"
+    df = pd.read_csv(file_path)
     nodes_a = df['Entrez Gene Interactor A']
     nodes_b = df['Entrez Gene Interactor B']
     # get a list of all the nodes in the network
@@ -21,10 +28,46 @@ def generate_PPI_network(data_set):
     return G
 
 
-def get_measures(operations, data_set):
+def get_measures(data_set):
     Graph = generate_PPI_network(data_set)
-    op_map = {
-        "shortest_path": find_shortest_path
-    }
+   # get_degreeCentrality(Graph)
+    #get_degreeOfNodes(Graph)
+    #get_betweennessCentrality(Graph)
+    get_closenessCentrality(Graph)
 
-    for operation in operations:
+
+
+#The following return a dict of nodes with their corresponding centrality values
+def get_degreeCentrality(NXGraph):
+    #The degree centrality values are normalized by dividing by the maximum possible degree in a simple graph n-1 where n is the number of nodes in G. So numbers can dodgy
+    degreeCentrality = nx.degree_centrality(NXGraph)
+    for k, v in degreeCentrality.items():
+        print("Node: {} => Degree Centrality: {}".format(k, v))
+
+def get_closenessCentrality(NXGraph):
+    #closenessCentrality = nx.closeness_centrality(NXGraph)
+    # for k, v in tqdm(closenessCentrality.items()):
+    #     print("Node: {} => Closeness Centrality: {}".format(k, v))
+
+    listOfNodes = NXGraph.nodes()
+    for node in tqdm(listOfNodes):
+        print("Node: {} => Closeness Centrality: {}".format(node,nx.closeness_centrality(NXGraph,node)))
+
+
+def get_betweennessCentrality(NXGraph):
+    betweennessCentrality = nx.betweenness_centrality(NXGraph)
+    for k, v in betweennessCentrality.items():
+        print("Node: {} => Betweenness Centrality: {}".format(k, v))
+
+def get_degreeOfNodes(NXGraph):
+    listOfNodes = NXGraph.nodes()
+    for node in listOfNodes:
+        print("Node: {} => Node Degree: {}".format(node,NXGraph.degree(node)))
+
+
+def mainFunctionInnitThatCallsTheOthers():
+    #write your fucking functi1on calls here
+    get_measures("C:/Users/shiva/Desktop/Data_Sets_Research/BIOGRID-Homosapien_UPDATED.csv")
+
+mainFunctionInnitThatCallsTheOthers()
+
