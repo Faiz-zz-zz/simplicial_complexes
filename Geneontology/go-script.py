aa76623ece1499aa468c5c42711a8194f3213e63
ReemmaMP <reemma.puredath@gmail.com>
2018-03-05 23:28:04 +0000

    should be able to generate matriz

--- Geneontology/go-script.py
+++ Geneontology/go-script.py
@@ -1,6 +1,7 @@
 import csv
 import pandas as pd
 import numpy
+import operator
 
 def convert_to_csv():
     with open('sgd_merged.txt') as fin, open('csv_format.csv', 'w') as fout:
@@ -22,7 +23,7 @@ def mapping():
     gene_df = df['DB_Object_Symbol']
     annotation_df = df['GO ID']
     exp_valid_df = df['Evidence']
-    map = {}
+    annotation_map = {}
     for i in range(0, no_of_rows):
         if(exp_valid_df[i] == 'EXP' or
         exp_valid_df[i] == 'IDA' or
@@ -30,33 +31,51 @@ def mapping():
         exp_valid_df[i] == 'IMP' or
         exp_valid_df[i] == 'IGI' or
         exp_valid_df[i] == 'IEP'):
-            if(gene_df[i] in map):
-                array = map[gene_df[i]]
+            if(gene_df[i] in annotation_map):
+                array = annotation_map[gene_df[i]]
                 if(annotation_df[i] not in array):
                     array.append(annotation_df[i])
-                    map[gene_df[i]] = array
+                    annotation_map[gene_df[i]] = array
             else:
-                map[gene_df[i]] = [annotation_df[i]]
+                annotation_map[gene_df[i]] = [annotation_df[i]]
     # print(gene_df[10], map[gene_df[10]])
-    return map
+    return annotation_map
 
 def generate_matrix():
     df = pd.read_csv('csv_format.csv', sep='\t')
     no_of_genes = df.shape[0]
     annotation_df = df['GO ID']
-    myset = set()
+
+    go_map = {}
     for i in range(0, no_of_genes):
-        myset.add(annotation_df[i])
-    no_of_annotations = len(myset)
-    # matrix = numpy.zeros((no_of_genes, no_of_annotations))    # can't use numpy cos array too big :( )
-    hashmap = mapping()
-    w, h = no_of_annotations, no_of_genes;
-    matrix = [[0 for x in range(w)] for y in range(h)]
-    for i in range(0, h):
-        for j in range(0, w):
-            if(annotation_df[j] in hashmap[i]):
+        if(annotation_df[i] in go_map):
+            counter = go_map[annotation_df[i]]
+            counter+=1
+            go_map[annotation_df[i]] = counter
+        else:
+            go_map[annotation_df[i]] = 1
+    sorted_x = sorted(go_map.items(), key=operator.itemgetter(1))
+    sorted_x = sorted_x[::-1][:100]
+    #go_id_list is the list of our popular GO:IDs
+    go_id_list = [x[0] for x in sorted_x]
+    no_go_ids = len(sorted_x)
+    annotation_map = mapping()
+    annotation_map_keys = list(annotation_map.keys())
+
+    w, h = len(annotation_map_keys), no_go_ids;
+    # fill it up with 0's
+    # matrix = [[0 for x in range(w)] for y in range(h)]
+    matrix = numpy.zeros((w,h))
+
+    for i in range(0, w):
+        for j in range(0, h):
+            # get the list of annotations from each gene
+            values = annotation_map[annotation_map_keys[i]]
+            # if the popular GO:ID matches any of the annotations associated with a gene
+            if(go_id_list[j] in values or go_id_list[j] == values):
                 matrix[i][j] = 1
-    print(matrix[0][0])
+                
+    print(matrix)
 
 # convert_to_csv()
 # mapping()
